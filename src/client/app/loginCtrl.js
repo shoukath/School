de4982acd5c10668136581ba04a39957316e167a
shoukath <shoukhan@yahoo.com>
2015-12-17 16:51:39 +0000

    Making the username and password dynamic now

--- src/client/app/loginCtrl.js
+++ src/client/app/loginCtrl.js
@@ -1,17 +1,28 @@
 'use strict'
 
+/* global angular */
+
 var loginApp = angular.module('loginApp', []);
 
 /*Login Application*/
 loginApp.controller('loginCtrl', function($scope, $http) {
-	// $scope.name = 'shoukath@gmail.com';
-	// $scope.password = 'test';
+	$scope.name = 'shoukathkhan';
+	$scope.password = 'test';
 	$scope.submit = function() {
 		console.log('Submit');
-		$http.get('/user').success(function(response) {
-			//console.log(response);
-			$scope.users = response;
+
+		$http({
+			url: '/login',
+			method: 'POST',
+			data: {
+				username: $scope.name,
+				password: $scope.password
+			}
+		}).success(function(data, status, headers, config) {
+			$scope.users = data;
+			console.log(data);
+		}).error(function(data, status, headers, config) {
+			$scope.status = status;
 		});
 	};
 });
-
