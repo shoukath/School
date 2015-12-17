9180cbf219ab598ad70e0d1f6d3bd57c096b3862
Shoukath Khan <shoukath.khan@orbitz.com>
2015-12-17 11:09:04 -0600

    Finally added a good watch task

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var gulp = require('gulp');
 // Include plugins
 var jshint = require('gulp-jshint'),
 	// concat = require('gulp-concat'),
-	useref = require('gulp-useref');
+	useref = require('gulp-useref'),
+	watch = require('gulp-watch');
 
 // Run JSHint
 gulp.task('lint', function() {
@@ -23,12 +24,17 @@ gulp.task('scripts', function() {
 		.pipe(gulp.dest('build/js'));
 });
 
-
 //gulp-useref
 gulp.task('useref', function() {
 	return gulp.src('src/client/*.html')
 		.pipe(useref())
 		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+	watch('src/client/*.html', function() {
+		gulp.run('useref');
+	});
 });
 
  // Default Task
